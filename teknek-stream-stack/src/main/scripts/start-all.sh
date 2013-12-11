2548a98966c3303ed55d0c082278b40b9d8af34e
Edward Capriolo <edlinuxguru@gmail.com>
2013-12-11 09:45:08 -0500

    Almost have the stream stack running

---
+++ teknek-stream-stack/src/main/scripts/start-all.sh
@@ -0,0 +1,6 @@
+CP=`ls teknek-stream-stack*.jar`
+CP=$CP:streamstack.properties
+for f in `ls lib/*` ; do
+  CP=$CP:$f
+done
+/usr/java/jdk1.7.0_13/bin/java -classpath $CP io.teknek.streamstack.StandAloneZooKeeperServer
