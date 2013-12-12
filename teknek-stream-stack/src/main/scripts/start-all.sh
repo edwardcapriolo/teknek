a5aa9012a4e06e20f661b6a8be5a1de521f101fd
Edward Capriolo <edlinuxguru@gmail.com>
2013-12-11 19:08:24 -0500

    Stand alone kafka starts horraywq

--- teknek-stream-stack/src/main/scripts/start-all.sh
+++ teknek-stream-stack/src/main/scripts/start-all.sh
@@ -3,4 +3,5 @@ CP=$CP:streamstack.properties
 for f in `ls lib/*` ; do
   CP=$CP:$f
 done
-/usr/java/jdk1.7.0_13/bin/java -classpath $CP io.teknek.streamstack.StandAloneZooKeeperServer
+/usr/java/jdk1.7.0_13/bin/java -classpath $CP io.teknek.streamstack.StandAloneZooKeeperServer &
+/usr/java/jdk1.7.0_13/bin/java -classpath $CP io.teknek.streamstack.StandAloneKafkaServer &
