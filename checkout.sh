4548259ae51c6bb5765a3ee8bc5c1688f3c83a54
Edward Capriolo <edlinuxguru@gmail.com>
2014-01-01 21:39:07 -0500

    Checkout all projects

---
+++ checkout.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/edwardcapriolo/teknek-core.git
+git clone https://github.com/edwardcapriolo/teknek-web.git
+git clone https://github.com/edwardcapriolo/teknek-cassandra.git
+git clone https://github.com/edwardcapriolo/teknek-kafka.git
+git clone https://github.com/edwardcapriolo/teknek-stream-stack.git
+git clone https://github.com/edwardcapriolo/teknek-hdfs.git
