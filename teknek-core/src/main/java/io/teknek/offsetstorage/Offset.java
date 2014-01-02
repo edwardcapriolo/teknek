2b3605119c3a10efca2e451240b3e9e7357dbef4
Edward Capriolo <edlinuxguru@gmail.com>
2014-01-01 21:10:41 -0500

    teknek-core has been moved to its own git repo

--- teknek-core/src/main/java/io/teknek/offsetstorage/Offset.java
+++
@@ -1,9 +0,0 @@
-package io.teknek.offsetstorage;
-
-public abstract class Offset {
-  public Offset(byte [] bytes){
-    
-  }
-  public abstract byte [] serialize();
-  
-}
--- teknek-core/src/main/scripts/start-teknek.sh
+++
@@ -1,5 +0,0 @@
-CP=`ls teknek-core*.jar`
-for f in `ls lib/*` ; do
-  CP=$CP:$f
-done
-/usr/java/jdk1.7.0_13/bin/java -classpath$CP io.teknek.daemon.TeknekDaemon
